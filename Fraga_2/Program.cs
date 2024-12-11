c08a36890efa6323d725d0458f8c2b0c1854cc21
ercr07001 <ercr07001@E-PW05CGR0>
2024-12-11 12:14:10 +0100

    Add project files.

---
+++ Fraga_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int input;
+
+        do
+        {
+            Console.Write("Skriv in ett heltal (0 för att avsluta): ");
+            input = int.Parse(Console.ReadLine());
+        } while (input != 0);
+
+        Console.WriteLine("Programmet avslutas.");
+
+        Console.ReadKey();
+    }
+}
